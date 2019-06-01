36c07455fbb3df82b72caec7b404b588a25f1cdc
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2019-06-01 13:47:51 -0400

    save as numpy arrays, train and test, with labels

--- parapair_sent_seq_data_generator.py
+++ parapair_sent_seq_data_generator.py
@@ -30,7 +30,7 @@ def main():
     train_elmo_lookup_file = sys.argv[2]
     test_parapair_file = sys.argv[3]
     test_elmo_lookup_file = sys.argv[4]
-    out_file = sys.argv[5]
+    outdir = sys.argv[5]
     train_elmo_lookup = np.load(train_elmo_lookup_file, allow_pickle=True)
     with open(train_parapair_file, 'r') as ppd:
         train_parapair_data = json.load(ppd)
@@ -39,22 +39,23 @@ def main():
         test_parapair_data = json.load(ppdt)
     elmo_vec_len = len(train_elmo_lookup[()][list(train_elmo_lookup[()].keys())[0]][0])
     print("ELMo vector length: {}".format(elmo_vec_len))
-    train_labels = train_parapair_data['labels']
+    train_labels = np.array(train_parapair_data['labels'])
+    train_labels = train_labels.reshape((train_labels.size, 1))
     train_sequences = []
     for pp in train_parapair_data['parapairs']:
         train_sequences.append(get_sequence_vec_parapair(pp, train_elmo_lookup, MAX_SENT_COUNT * elmo_vec_len))
     train_sequences = np.array(train_sequences)
-    test_labels = test_parapair_data['labels']
+    test_labels = np.array(test_parapair_data['labels'])
+    test_labels = test_labels.reshape((test_labels.size, 1))
     test_sequences = []
     for pp in test_parapair_data['parapairs']:
         test_sequences.append(get_sequence_vec_parapair(pp, test_elmo_lookup, MAX_SENT_COUNT * elmo_vec_len))
     test_sequences = np.array(test_sequences)
-    sequence_data = dict()
-    sequence_data['Xtrain'] = train_sequences
-    sequence_data['ytrain'] = train_labels
-    sequence_data['Xtest'] = test_sequences
-    sequence_data['ytest'] = test_labels
-    np.save(out_file, sequence_data)
+    print("train seq shape {}".format(train_sequences.shape)+" train labels shape {}".format(train_labels.shape))
+    train_dat = np.hstack((train_sequences, train_labels))
+    test_dat = np.hstack((test_sequences, test_labels))
+    np.save(outdir+"/train", train_dat)
+    np.save(outdir+"/test", test_dat)
 
 if __name__ == '__main__':
     main()

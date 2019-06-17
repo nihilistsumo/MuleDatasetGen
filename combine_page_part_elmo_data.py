1694527d09d8c1bbbb86333f273d793954d693c1
Sumanta  Kashyapi <sk1105@wildcats.unh.edu>
2019-06-17 14:53:42 -0400

    add combine elmo dicts

---
+++ combine_page_part_elmo_data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import math, json, os, sys, argparse
+import numpy as np
+
+#######################################
+#
+# Combine pagewise elmo data generated
+# by elmo_embedding_generator.py
+#
+#######################################
+
+indir = sys.argv[1]
+outfile = sys.argv[2]
+
+comb_dict = dict()
+for f in os.listdir(indir):
+    print(f)
+    curr_dict = np.load(indir+"/"+f)[()]
+    comb_dict.update(curr_dict)
+np.save(outfile, comb_dict)
